98f3a8c57db3888a0e3161152e05276773717c18
its-kaushik <mkaushik0033@gmail.com>
2023-03-01 17:40:44 +0530

    Creating navbar component

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,27 @@
+import { Link } from 'react-router-dom';
+
+import './Navbar.scss';
+
 const Navbar = () => {
     return (
-        <h1>This is NAVBAR</h1>
+        <div className='navbar' >
+            <div className='wrapper'>
+                <div className='left' >
+                </div>
+                <div className='right'>
+                    <div className='item' >
+                        <Link>About Us</Link>
+                    </div>
+                    <div className='item' >
+                        <Link>Best Sellers</Link>
+                    </div>
+                    <div className='item' >
+                        <Link>Products</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
-}
+};
 
 export default Navbar ;
